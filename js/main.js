a728da211e4b8cdd352a0633082fc7d3efa418cf
Albert Martinez <ialbertmartinez@outlook.com>
2024-07-31 23:07:12 -0700

    add animation to game part 1: falling jade

--- js/main.js
+++ js/main.js
@@ -1,20 +1,25 @@
 window.onload = function(){
-    // console.log("code check");
     const game = document.getElementById("game");
     const mFrameId = requestAnimationFrame(makeJade);
-    
+   
     function makeJade(x) {
         var top = 0;
         const jade = document.createElement('div');
         jade.className = "jade";
         jade.style.left = `${x}px`;
         jade.style.top = 0;
-
         game.appendChild(jade);
         
-
+        let jFrameId = requestAnimationFrame(moveJade);
+        
+        function moveJade(){
+            top += 2;
+            jade.style.top = `${top}px`;
+            jFrameId = requestAnimationFrame(moveJade);
+        }
+        // moveJade()
     }
-    makeJade(randomNumber());
+    mFrameId = makeJade(randomNumber());
 
 }
 
